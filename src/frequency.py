64187e474915fecc1fdec1b9d58d44bfc2ca844c
linhr <linhr10@gmail.com>
2015-05-31 17:26:49 -0700

    Implement data frame cache

--- src/frequency.py
+++ src/frequency.py
@@ -6,28 +6,30 @@ from bidder import *
 from utils import *
 
 @use_bids_data
+@cacheable_data_frame('attributes/{column}.csv', '{column}')
 def attribute_freq(conn, column):
     sql = 'SELECT {0}, COUNT(*) AS n FROM bids GROUP BY {0}'.format(column)
     counts = pd.read_sql(sql, conn, index_col=column).sort('n')
-    return counts['n']
+    return counts
+
+def get_popular_auctions(count=None):
+    auctions = attribute_freq('auction')
+    if count is not None:
+        auctions = auctions[-count:]
+    return list(auctions.index.values)
 
 @use_bids_data
 def bidder_per_auction_freq(conn, column, auction_count=100):
     bidders = get_bidders()
-    auctions = attribute_freq('auction')[-auction_count:]
+    auctions = get_popular_auctions(auction_count)
     df = pd.DataFrame(data=None, index=bidders.index)
-    for auction in auctions.index:
+    for auction in auctions:
         sql = ("SELECT bidder_id, COUNT(DISTINCT {0}) as n FROM bids "
             "WHERE auction = '{1}' GROUP BY bidder_id").format(column, auction)
         df[auction] = pd.read_sql(sql, conn, index_col='bidder_id')
     return df
 
-@use_workspace_directory('attributes')
-def save_attribute_freq():
+if __name__ == '__main__':
     columns = ('auction', 'bidder_id', 'merchandise', 'device', 'country', 'ip', 'url')
     for column in columns:
-        path = workspace_file('attributes/{0}.csv'.format(column))
-        attribute_freq(column).to_csv(path, index=True, header=True)
-
-if __name__ == '__main__':
-    save_attribute_freq()
+        attribute_freq(column)
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,11 @@ import os
 import os.path
 import errno
 import functools
+import gzip
+import inspect
 import sqlite3
+
+import pandas as pd
 
 import settings
 
@@ -34,5 +38,37 @@ def use_workspace_directory(path):
         def wrapper(*args, **kwargs):
             try_makedirs(workspace_file(path))
             return func(*args, **kwargs)
+        return wrapper
+    return wrapper_outer
+
+def cacheable_data_frame(pathfmt, indexfmt):
+    def format_index_col(index_col_fmt, *args, **kwargs):
+        isstr = lambda x: isinstance(x, basestring)
+        formatted = lambda x: x.format(*args, **kwargs)
+        if isstr(index_col_fmt):
+            return formatted(index_col_fmt)
+        elif isinstance(index_col_fmt, (list, tuple)):
+            return [formatted(x) if isstr(x) else x for x in index_col_fmt]
+        return index_col_fmt
+
+    def save_csv(df, full_path):
+        try_makedirs(os.path.dirname(full_path))
+        if full_path.lower().endswith('.gz'):
+            with gzip.open(full_path, 'wb') as output:
+                df.to_csv(output, index=True, header=True)
+        else:
+            df.to_csv(full_path, index=True, header=True)
+
+    def wrapper_outer(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            fmtargs = inspect.getcallargs(func, *args, **kwargs)
+            full_path = workspace_file(pathfmt.format(**fmtargs))
+            if os.path.exists(full_path):
+                index_col = format_index_col(indexfmt, **fmtargs)
+                return pd.read_csv(full_path, index_col=index_col)
+            result = func(*args, **kwargs)
+            save_csv(result, full_path)
+            return result
         return wrapper
     return wrapper_outer
